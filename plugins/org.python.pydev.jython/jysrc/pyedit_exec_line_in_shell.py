c36ff2e5020d13f1adcf5fcdd04f4f0f8c8b1007
Ewan Higgs <ehiggs@ahl.com>
2012-02-20 15:15:05 +0000

    AHLRAP-1371 AHLRAP-1366 Fixes to F1 Console submission
    This is a squash of 5 commits.
    
    This is a fix for AHLRAP-1373 'If you change the console too often, F1
    stops working'. The problem was that the exec line in shell module would
    raise an exception if an appropriate console was not available for
    command submission. Once the module had the raised exception, I guess
    eclipse doesn't reinitialise it again so one needs to restart PyDev to
    get F1 functionality back.
    
    I fixed this by merely using a try-except block to catch any exceptions.
    
    AHLRAP-1371 AHLRAP-1366 Fixes to F1 Console submission
    
    This patch carries a few changes:
    
    - Fix for AHLRAP-1371: When the Scripting Console was visible, the code
    submission would send the code to a view only window and this resulted
    in an unhandled exception. This exception broke the script and it
    wouldn't work again. The change I have made fixes this by handling the
    exception; but also raising a console window so the code can be
    submitted.
    
    - Fix for AHLRAP-1366: When stepping through the code using F1, when the
    user reaches the end of the file, we don't want to wrap around to the
    beginning. It makes no sense to jump all the way back to the import
    statements once the module is done running. This fix was done by
    changing how the goto_next_line functions works. It now returns a
    boolean for whether or not is actually moved to the next line.
    
    - Changing some function names to use pythonic_naming_style rather than
    javaNamingStyle.
    
    - Some basic dead code removal.
    
    - There is a lot of code that could be tidied up here, but I have
    refrained from going carte blanche and renaming all functions which are
    not called externally to have a _prefix; or removing comments and
    replacing them with extensive docstrings. This module could use some
    love.
    
    Cosmetic changes.
    
    - Renamed variables to reflect their privacy.
    
    - Added some meagre documentation.
    
    Removed re-raise which was incorrectly commited previously.
    
    Protect from F1 submission when no Console is set.
    
    James Blackburn found that when the Scripting Console is set
    and a file is opened, and F1 is hit, an exception was thrown as
    no Console was found. Unfortunately, the IConsoleView doesn't
    appear to provide a way to find anything but the top of the
    IConsole list; so I've just protected it now.
    
    Also, to aid readability, I've inverted some if statements to
    return and reduced the nesting level for _get_console_parts and
    _show_console.
    
    AHLRAP-1366 - Fixed the F1 loop around
    
    This is a subsequent fix to the previous commit now using the
    last_line calculated from the document.
    
    AHLRAP-1366 - Do not iterate through file when no Console is available.
    
    Change-Id: Ic190b8b51110fa764dbbcd7f32f4d9fc006e6f15

--- plugins/org.python.pydev.jython/jysrc/pyedit_exec_line_in_shell.py
+++ plugins/org.python.pydev.jython/jysrc/pyedit_exec_line_in_shell.py
@@ -1,16 +1,9 @@
-if False:
-    from org.python.pydev.editor import PyEdit #@UnresolvedImport
-    cmd = 'command string'
-    editor = PyEdit
-
 #--------------------------------------------------------------- REQUIRED LOCALS
 #interface: String indicating which command will be executed #As this script will be watching the PyEdit (that is the actual editor in Pydev), and this script
 #will be listening to it, this string can indicate any of the methods of org.python.pydev.editor.IPyEditListener
 assert cmd is not None
 
 #interface: PyEdit object: this is the actual editor that we will act upon assert editor is not None
-
-#---- Uncomment the line below to see the outputs received
 
 import sys
 print 'Command: ' + cmd
@@ -70,186 +63,261 @@ class DoTopCommandJob(UIJob):
         self.setPriority(Job.SHORT)
         
     def runInUIThread(self, progress_monitor):
-        self.executor.do_top_command()
+        try:
+            self.executor._do_top_command()
+        except:
+            # This can be benign if, e.g. the Script Console has focus.
+            pass
         return Status.OK_STATUS
 
-# Code to execute a line
 class ExecuteLine(Action):
+    '''
+    Code to execute a line
+    '''
     def __init__(self, editor=None):
         Action.__init__(self)
-        self.editor = editor
-        self.console = None
-        self.console_listener = ConsoleDocumentListener(self)
-        self.commands=[]
-        self.current_block_indent = None
-        self.prev_line_indent = None
+        self._editor = editor
+        self._console = None
+        self._console_listener = ConsoleDocumentListener(self)
+        self._commands = []
+        self._current_block_indent = None
+        self._prev_line_indent = None
 
-    def getConsoleParts(self, page, restore):
-        # logic taken from EvaluationActionSetter.java
-        consoleParts = []
+    def _get_console_view_parts(self, page, restore):
+        '''
+        Find the Console Pages within the Console View.
+        Logic taken from EvaluationActionSetter.java
+        '''
+        console_view_parts = []
         viewReferences = page.getViewReferences()
         for ref in viewReferences:
-            if ref.getId() == IConsoleConstants.ID_CONSOLE_VIEW:
-                part = ref.getView(restore)
-                if part is not None:
-                    consoleParts.append(part)
-                    if restore:
-                        return consoleParts
-        return consoleParts;
+            if ref.getId() != IConsoleConstants.ID_CONSOLE_VIEW:
+                continue
 
-    def showConsole(self):
-        # logic derived from EvaluationActionSetter.java
+            view_part = ref.getView(restore)
+            if view_part is None:
+                continue
+            
+            console_view_parts.append(view_part)
+            if restore:
+                return console_view_parts
+
+        return console_view_parts;
+
+    def _show_console(self):
+        '''
+        Raise the Console Page from within the Console View
+        Logic derived from EvaluationActionSetter.java
+        '''
         window = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-        if window is not None:
-            page = window.getActivePage()
-            if page is not None:
-                consoleParts = self.getConsoleParts(page, False)
-                if len(consoleParts) == 0:
-                    consoleParts = self.getConsoleParts(page, True)
-#                if len(consoleParts) > 1:
-#                    MessageDialog.openInformation(editor.getSite().getShell(), "Execute line", "Too many consoles open");
-
-                for part in consoleParts:
-                    if part.getConsole().getType() == PydevConsoleConstants.CONSOLE_TYPE:
-                        self.set_console(part.getConsole())
-#                        part.display(self.console)
-        return
-
-    def create_console(self):
-        self.set_console(PydevConsoleFactory().createConsole())
+        if window is None:
+            return False
         
-    def set_console(self,console):
-        self.console = console
+        page = window.getActivePage()
+        if page is None:
+            return False
 
-    def get_newline(self):
-        return PyAction.getDelimiter(self.editor.getDocument())
+        console_view_parts = self._get_console_view_parts(page, False)
+        if len(console_view_parts) == 0:
+            console_view_parts = self._get_console_view_parts(page, True)
 
-    def get_selection(self):
-        return PySelection(self.editor).getSelectedText()
+        for view_part in console_view_parts:
+            if view_part.getConsole().getType() == PydevConsoleConstants.CONSOLE_TYPE:
+                self._set_console(view_part.getConsole())
+            elif self._console is not None:
+                page.bringToTop(view_part)
+                view_part.display(self._console)
+            else: return False
+            
+        return True
 
-    def send_to_console(self, text):
+    def _get_console(self):
+        '''
+        Resolve the appropriate console for code submission.
+        If a console doesn't exist; create it.
+        If a console was closed; reopen it.
+        If the current console isn't a PyDev Console; raise the most 
+        recent PyDev Console
+        '''
+        # create console on first use
+        if self._console is None or self._console.getViewer() is None:
+            self._create_console()
+
+        # if console has been closed then recreate
+        if self._console.getViewer().getDocument() is None:
+            self._create_console()
+        return self._console
+
+    def _create_console(self):
+        '''
+        If a Console page within the Console View doesn't exist, we need 
+        to create one.
+        '''
+        self._set_console(PydevConsoleFactory().createConsole())
+        
+    def _set_console(self, console):
+        '''
+        Sets the current Console to the current page if the current page if it's 
+        actually a Console Page; otherwise we can use the previous one.
+        
+        Test: If the Scripting Console is visible and the previous Console page is 
+        removed; verify this works.
+        '''
+        if not console is None:
+            self._console = console
+
+    def _should_bring_to_top(self, console, view):
+        '''
+        Returns whether the given console view should be brought to the top.
+        The view should not be brought to the top if the view is pinned on
+        a console other than the given console.
+        '''
+        retval = True
+        if view.isPinned():
+            pinned_console = view.getConsole()
+            retval = self._console == pinned_console
+        return retval
+        
+    def _get_newline(self):
+        return PyAction.getDelimiter(self._editor.getDocument())
+
+    def _get_selection(self):
+        return PySelection(self._editor).getSelectedText()
+
+    def _send_to_console(self, text):
         if len(rstrip(text)):
-            self.commands.append( text ) 
-            if len(self.commands)==1:
+            self._commands.append( text ) 
+            if len(self._commands)==1:
                 job = DoTopCommandJob(self)
                 job.schedule()
                 
-    def do_top_command(self):
-        document = self.console.getDocument()
-        text = self.commands[0]
-        document.addDocumentListener(self.console_listener)
-        self.console_listener.lines_to_process = text.count('\n')
+    def _do_top_command(self):
+        document = self._console.getDocument()
+        text = self._commands[0]
+        document.addDocumentListener(self._console_listener)
+        self._console_listener.lines_to_process = text.count('\n')
         document.replace(document.getLength(), 0, text)
     
     def complete_top_command(self):
-        self.console.getDocument().removeDocumentListener(self.console_listener)
-        self.commands = self.commands[1:]
-        if len(self.commands) > 0 :
+        self._console.getDocument().removeDocumentListener(self._console_listener)
+        self._commands = self._commands[1:]
+        if len(self._commands) > 0 :
             job = DoTopCommandJob(self)
             job.schedule()
 
-    def reset_line_state(self):
-        self.current_block_indent = None
-        self.prev_line_indent = None
+    def _reset_line_state(self):
+        self._current_block_indent = None
+        self._prev_line_indent     = None
 
-    def get_next_line(self):
-        selection = PySelection(self.editor).getLine()
+
+    def _get_line(self):
+        '''
+        Find the current line.
+        '''
+        selection = PySelection(self._editor).getLine()
         # strip tailing whitespace
         return rstrip(selection)
 
-    def goto_next_line(self):
-        # skip cursor to next line
-        oSelection = PySelection(self.editor)
-        offset = oSelection.getLineOffset(oSelection.getCursorLine() + 1)
-        self.editor.setSelection(offset, 0)
+    def _goto_next_line(self):
+        '''
+        Find the next line. Return if there was a new line to traverse to.
+        Note: the selection system appears to wrap around to the beginning if 
+        the line is incremented past the end. No user wants to go back to imports 
+        once they've completed their step-through, so we protect against that.
+        '''
 
-    def should_skip(self, line):
+        # skip cursor to next line
+        oSelection = PySelection(self._editor)
+        current_line = oSelection.getCursorLine()
+        last_line = oSelection.getDoc().getNumberOfLines()-1
+        offset = oSelection.getLineOffset(current_line + 1)
+        if current_line == last_line:
+            return False
+        self._editor.setSelection(offset, 0)
+        return True
+
+    def _should_skip(self, line):
         return len(line) == 0 or RE_COMMENT.match(line)
 
-    def run_selection_mode(self, selection):
-        """ User has selected a block of text and hit F1
-        """
-        self.reset_line_state()
+    def _run_selection_mode(self, selection):
+        '''
+        User has selected a block of text and hit F1
+        '''
+        self._reset_line_state()
         if selection[-1]=='\n' :
             text = selection
         else :
             text = selection + '\n'
-        self.send_to_console(text)
+        self._send_to_console(text)
 
-    def run_line_mode(self):
-        """ User is running through the code line by line
-        """
+    def _run_line_mode(self):
+        ''' 
+        User is running through the code line by line
+        '''
         # Save away the current line which we'll send to the console
-        current_line = self.get_next_line()
+        current_line = self._get_line()
 
         # Skip through to the next non-blank line
-        self.goto_next_line()
-        next_line = self.get_next_line()
-        while self.should_skip(next_line):
-            self.goto_next_line()
-            next_line = self.get_next_line()
+        self._goto_next_line()
+        next_line = self._get_line()
+        while self._should_skip(next_line) and self._goto_next_line():
+            next_line = self._get_line()
 
         # Look-ahead for indentation changes
         indentation = len(next_line) - len(next_line.lstrip())
-        if self.current_block_indent is None:
+        if self._current_block_indent is None:
             # We're starting a new block which might not be at indent 0
-            self.current_block_indent = indentation
-            self.prev_line_indent = indentation
+            self._current_block_indent = indentation
+            self._prev_line_indent = indentation
 
-        if indentation <= self.current_block_indent and indentation != self.prev_line_indent:
+        if indentation <= self._current_block_indent and indentation != self._prev_line_indent:
             # We've finished a block - need to send 2 newlines to IPython to tell it to
             # close the block. Don't do this though if we're tracking the same level
             # of indentation.
-            self.current_block_indent = indentation
-            self.prev_line_indent = indentation
+            self._current_block_indent = indentation
+            self._prev_line_indent = indentation
             # Current line gets an extra newline to terminate the block
-            current_line = "%s%s" % (current_line, 2 * self.get_newline())
+            current_line = "%s%s" % (current_line, 2 * self._get_newline())
 
-        self.prev_line_indent = indentation
+        self._prev_line_indent = indentation
 
         # add newline
-        current_line += self.get_newline()
+        current_line += self._get_newline()
 
         # send command to console
-        self.send_to_console(current_line)
+        self._send_to_console(current_line)
 
     def run(self):
-#        # create console on first use
-#        if self.console is None or self.console.getViewer() is None:
-#            self.create_console()
+        # If there's no console, don't do anything else.
+        if not self._show_console():
+            return
 
-#        # if console has been closed then recreate
-#        if self.console.getViewer().getDocument() is None:
-#            self.create_console()
-        self.showConsole()
-
-        selection = self.get_selection()
+        selection = self._get_selection()
 
         if not len(selection) == 0:
             # User has selected a block of text
-            self.run_selection_mode(selection)
+            self._run_selection_mode(selection)
         else:
             # User has no selection, use line-by-line mode
-            self.run_line_mode()
+            self._run_line_mode()
             
     def unhook(self):
-        if self.console:
-            self.console.getDocument().removeDocumentListener(self.console_listener)
+        if self._console:
+            self._console.getDocument().removeDocumentListener(self._console_listener)
         
-
 def bindInInterface():
         # Cribbed from http://eclipse-pydev.sourcearchive.com/documentation/1.2.5/pyedit__next__problem_8py-source.html
         #bind the action to some internal definition
         action = ExecuteLine(editor)
 
-        #ok, the plugin.xml file defined a command and a binding with the string from COMMAND_ID.
+        #The plugin.xml file defined a command and a binding with the string from COMMAND_ID.
         #by seting the action definition id and the id itself, we will bind this command to the keybinding defined
         #(this is the right way of doing it, as it will enter the abstractions of Eclipse and allow the user to
         #later change that keybinding).
         action.setActionDefinitionId(COMMAND_ID)
         action.setId(COMMAND_ID)
         try:
-            #may happen because we're starting it in a thread, so, it may be closed before
+            #We're starting in a thread, so, it may be closed before
             #we've the change to bind it
             last_execute_line = editor.getAction(COMMAND_ID)
             if last_execute_line :
